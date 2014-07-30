6ea6cfdbed3e7e94c96dbef342db6299814bd276
Minh Tue Vo <mvo@edx.org>
2014-07-30 16:27:40 -0400

    adding blacklisting

--- tutorial.py
+++ tutorial.py
@@ -3,15 +3,14 @@ from pprint import pprint
 server = Server("/Users/minhtuevo/browsermob-proxy/bin/browsermob-proxy")
 server.start()
 proxy = server.create_proxy()
+proxy.blacklist("http://www\.bing\.com/", 404)
 
 from selenium import webdriver
 profile  = webdriver.FirefoxProfile()
 profile.set_proxy(proxy.selenium_proxy())
 driver = webdriver.Firefox(firefox_profile=profile)
 
-proxy.new_har("google")
-driver.get("http://www.google.co.uk")
-pprint(proxy.har) # returns a HAR JSON blob
+driver.get("http://www.bing.com")
 
 proxy.stop()
 driver.quit()
